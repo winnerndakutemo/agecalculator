8668264269702459fcc85cd2cde42e1bb35aac56
Winner <ndakutemow@gmail.com>
2024-10-31 14:43:22 +0100

    validation, calculation, and visualisation of age

--- js/app.js
+++ js/app.js
@@ -21,18 +21,26 @@ inputs.forEach((input) => {
         input.classList.remove("input-error");
       });
     }
-
-    if (!isValid(e.target)) {
+    if (!areEmptyFields(inputs) && isValid(e.target)) {
+      //Calculate date
+      const [day, month, year] = [
+        inputs[0].value,
+        inputs[1].value,
+        inputs[2].value,
+      ];
+      const age = getAge(year, month, day);
+      insertAge(age);
+    } else if (!isValid(e.target)) {
       const paragraph = e.target.parentElement.querySelector(".error");
       paragraph.classList.remove("error-hidden");
       paragraph.innerHTML = "Enter a valid " + e.target.id;
       e.target.classList.add("input-error");
-    } else {
-      //revert changes
-      const paragraph = e.target.parentElement.querySelector(".error");
-      paragraph.classList.add("error-hidden");
-      paragraph.innerHTML = "Enter a valid " + e.target.id;
-      e.target.classList.remove("input-error");
+      insertAge({
+        years: "--",
+        months: "--",
+        days: "--",
+      });
+      return;
     }
   });
 });
@@ -71,11 +79,42 @@ function areEmptyFields(inputs) {
 
 function isValid(input) {
   if (input.id === "day") {
-    ("");
     return input.value <= 31 && input.value >= 1 ? true : false;
   } else if (input.id === "month") {
     return input.value <= 12 && input.value >= 1 ? true : false;
   } else if (input.id === "year") {
-    return input.value >= 1 ? true : false;
+    return input.value >= 1000 ? true : false;
   }
+}
+
+function getAge(year, month, day) {
+  const birthDate = new Date(year, month - 1, day);
+  const today = new Date();
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+  const dayDiff = today.getDate() - birthDate.getDate();
+  if (monthDiff < 0) {
+    age--;
+    monthDiff += 12;
+  }
+
+  if (dayDiff < 0) {
+    monthDiff--;
+    const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+    dayDiff += lastMonth.getDate();
+  }
+  return {
+    years: age,
+    months: monthDiff,
+    days: dayDiff,
+  };
+}
+
+function insertAge(age) {
+  const yearsOutput = document.querySelector(".years-output");
+  const monthsOutpout = document.querySelector(".months-output");
+  const daysOutput = document.querySelector(".days-output");
+  daysOutput.innerHTML = age.days;
+  monthsOutpout.innerHTML = age.months;
+  yearsOutput.innerHTML = age.years;
 }
